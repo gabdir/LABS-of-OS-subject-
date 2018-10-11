b628fe8e43002b15c4f7fbca8c0c79c2459b47b9
gabdir <35723166+gabdir@users.noreply.github.com>
2018-10-11 20:26:25 +0300

    Add files via upload

---
+++ week8/ex2.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char const *argv[])
+{
+	int k = 0;
+	int **n =  malloc(10 * sizeof(int));
+
+	while(k<10){
+
+		*(n+k) = malloc(2621440*sizeof(int));
+
+		memset(*(n+k),0,2621440*sizeof(int));
+		k++;
+
+		sleep(1);
+	}
+	return 0;
+}
---
+++ week8/ex4.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/resource.h>
+#include <errno.h>
+
+int main(int argc, char const *argv[])
+{
+	int k = 0;
+	int **n =  malloc(10 * sizeof(int));
+
+	while(k<10){
+
+		*(n+k) = malloc(2621440*sizeof(int));
+
+		memset(*(n+k),0,2621440*sizeof(int));
+		
+		struct rusage* usage = malloc(sizeof(struct rusage)); 
+		getrusage(RUSAGE_SELF, usage);
+
+		if(errno == EFAULT)
+    		printf("Error: EFAULT\n");
+		else if(errno == EINVAL)
+    		printf("Error: EINVAL\n");
+
+    	printf("Max: %ld\n",usage -> ru_maxrss);
+
+    	k++;
+		sleep(1);
+	}
+	return 0;
+}
