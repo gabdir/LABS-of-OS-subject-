c84cd0db0a306fcac3bcace2025f0f6884e2da9f
gabdir <35723166+gabdir@users.noreply.github.com>
2018-10-11 20:37:32 +0300

    Update ex4.c

--- week8/ex4.c
+++ week8/ex4.c
@@ -10,8 +10,8 @@ int main(int argc, char const *argv[])
 	int k = 0;
 	int **n =  malloc(10 * sizeof(int));
 
-	while(k<10){
-
+	while(k<10)
+	{
 		*(n+k) = malloc(2621440*sizeof(int));
 
 		memset(*(n+k),0,2621440*sizeof(int));
