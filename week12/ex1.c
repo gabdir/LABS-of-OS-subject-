eb4314a47d2269728f223cc9584bbf7d0caefe5d
gabdir <35723166+gabdir@users.noreply.github.com>
2018-11-08 22:24:41 +0300

    Add files via upload

---
+++ week12/ex1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define string_size 32
+
+int main() {
+
+  int f = open("/dev/random", O_RDONLY);
+  
+  if (f < 0) 
+  {
+    return -1;
+  } 
+  else 
+  {
+
+    char array[string_size + 1];
+    int res = read(f, array, string_size);
+    
+    if (res < 0) 
+    {
+      return -1;
+    } 
+    else 
+    {
+      printf("%s\n", array);
+    }
+  }
+
+  close(f);
+  return 0;
+}
